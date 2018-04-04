40e5caf49294818e5d8c433c3f6b00069e91b2b9
Jermaine Walker <jermainemwalker@gmail.com>
2018-04-04 16:19:34 -0700

    Finished CSS, and completed first do...while loop.

--- js/app.js
+++ js/app.js
@@ -1,3 +1,29 @@
-body {
-  background: rgb(22, 25, 28);
-}
+'use strict';
+
+// var attempts = 0;
+// var ageGuess1 = prompt( 'Do you think I am older than 20? Please answer \'yes\' or \'no\'.' ).toLowerCase();
+
+// // while ( flag === 'false' && attempts < 5 ) {
+// //   if ( ageGuess1 === 'yes' ) {
+// //     alert( 'Nice! You\'re right. Let\'s keep going.');
+// //   } else if ( ageGuess1 === 'no' ) {
+
+// //     flag = false;
+// //     attempts++;
+// //   } else {
+// //     alert( 'Please answer \'yes\' or \'no\'.');
+// //   }
+// // }
+
+do {
+  var ageGuess1 = prompt( 'Do you think I am older than 20? Please answer \'yes\' or \'no\'.' ).toLowerCase();
+  if ( ageGuess1 === 'yes' ) {
+    alert( 'Correct! Let\'s keep going.' );
+    break;
+  } else if ( ageGuess1 === 'no' ) {
+    alert( 'Ah, nope! Let\'s start over, shall we?' );
+    var ageCorrect = false;
+  } else {
+    alert( 'Please answer \'yes\' or \'no\'.' );
+  }
+} while ( ageCorrect === false );
