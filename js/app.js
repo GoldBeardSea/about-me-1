be05afdd5b7437c06a313b8934e8368ac0d35243
Jermaine Walker <jermainemwalker@gmail.com>
2018-04-04 16:45:10 -0700

    Fixed problem with logic after correcting user input.

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 'use strict';
 
 do {
-  var ageGuess1 = prompt( 'Do you think I am older than 20? Please answer \'yes\' or \'no\'.' ).toLowerCase();
+  var ageGuess1 = prompt( 'Do you think I am older than 20?' ).toLowerCase();
   if ( ageGuess1 === 'yes' ) {
     alert( 'Good start! Let\'s keep going.' );
     break;
@@ -10,11 +10,12 @@ do {
     var ageCorrect1 = false;
   } else {
     alert( 'Please answer \'yes\' or \'no\'.' );
+    ageCorrect1 = false;
   }
 } while ( ageCorrect1 === false );
 
 do {
-  var ageGuess2 = prompt( 'Do you think I am older than 25? Please answer \'yes\' or \'no\'.' ).toLowerCase();
+  var ageGuess2 = prompt( 'Do you think I am older than 25?' ).toLowerCase();
   if ( ageGuess2 === 'yes' ) {
     alert( 'Nice! Doing great. Keep going, brainiac!' );
     break;
@@ -23,11 +24,12 @@ do {
     var ageCorrect2 = false;
   } else {
     alert( 'Please answer \'yes\' or \'no\'.' );
+    ageCorrect2 = false;
   }
 } while ( ageCorrect2 === false );
 
 do {
-  var ageGuess3 = prompt( 'Do you think I am older than 30? Please answer \'yes\' or \'no\'.' ).toLowerCase();
+  var ageGuess3 = prompt( 'Do you think I am older than 30?' ).toLowerCase();
   if ( ageGuess3 === 'yes' ) {
     alert( 'Yes! Hey, you\'re getting the hang of this.' );
     break;
@@ -36,5 +38,20 @@ do {
     var ageCorrect3 = false;
   } else {
     alert( 'Please answer \'yes\' or \'no\'.' );
+    ageCorrect3 = false;
   }
 } while ( ageCorrect3 === false );
+
+do {
+  var ageGuess4 = prompt( 'Do you think I am older than 35?' ).toLowerCase();
+  if ( ageGuess4 === 'yes' ) {
+    alert( 'Yes! Hey, you\'re getting the hang of this.' );
+    break;
+  } else if ( ageGuess4 === 'no' ) {
+    alert( 'Oof. Try again, buddy.' );
+    var ageCorrect4 = false;
+  } else {
+    alert( 'Please answer \'yes\' or \'no\'.' );
+    ageCorrect4 = false;
+  }
+} while ( ageCorrect4 === false );
